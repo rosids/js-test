5acae7429dc54ddc63296a980f348e89d3e1233e
Rosiele David <rosieleserpa@hotmail.com>
2021-05-17 20:38:18 -0300

    correçao da rota de login

--- js-test/src/App.js
+++ js-test/src/App.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import GlobalProvider from './context/GlobalConsumer';
 import Home from './pages/Home';
 import Login from './pages/Login';
 
 function App() {
   return (
     <BrowserRouter>
-      <Switch>
-        <Route path="/" component={Login} />
-        <Route path="/projects" component={Home} />
-      </Switch>
+      <GlobalProvider>
+        <Switch>
+          <Route exact path="/" component={Login} />
+          <Route path="/projects" component={Home} />
+        </Switch>
+      </GlobalProvider>
     </BrowserRouter>
   );
 }
