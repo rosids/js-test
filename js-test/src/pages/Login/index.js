fdb7db5d280978595242fef2d976114cfddcad6e
Rosiele David <rosieleserpa@hotmail.com>
2021-05-17 18:14:42 -0300

    criaçao da tela de login

--- js-test/src/pages/Login/index.js
+++ js-test/src/pages/Login/index.js
@@ -1,7 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './index.css';
 
 export default function Login() {
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const handleEmail = ({ target }) => setEmail(target.value);
+  const handlePassword = ({ target }) => setPassword(target.value);
+
   return (
-    <div>Login</div>
+    <form className="form">
+      <label htmlFor="email">
+        Email:
+        <input
+          type="email"
+          name="email"
+          value={email}
+          id="email"
+          onChange={handleEmail}
+        />
+      </label>
+      <label htmlFor="password">
+        Senha:
+        <input
+          type="password"
+          name="password"
+          value={password}
+          id="password"
+          onChange={handlePassword}
+        />
+      </label>
+    </form>
   );
 }
