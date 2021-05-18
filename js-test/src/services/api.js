1e81d434d3649be2f68aa3adcc0c1e7090e98df1
Rosiele David <rosieleserpa@hotmail.com>
2021-05-18 11:05:43 -0300

    criar request de api de commits, branches, ultima atualizaçao

--- js-test/src/services/api.js
+++ js-test/src/services/api.js
@@ -1,6 +1,19 @@
-async function requestRepos() {
+export async function requestRepos() {
   const results = await (await fetch('https://api.github.com/users/rosids/repos')).json();
   return results;
 }
 
-export default requestRepos;
+export async function requestRepoLastUpdate(name) {
+  const results = await (await fetch(`https://api.github.com/repos/rosids/${name}`)).json();
+  return results.updated_at;
+}
+
+export async function requestRepoCommits(name) {
+  const results = await (await fetch(`https://api.github.com/repos/rosids/${name}/commits`)).json();
+  return results;
+}
+
+export async function requestRepoBranches(name) {
+  const results = await (await fetch(`https://api.github.com/repos/rosids/${name}/branches`)).json();
+  return results;
+}
