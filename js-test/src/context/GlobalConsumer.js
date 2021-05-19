86ce2fa7f4479a779d0164b0e79422262cdc2a5e
Rosiele David <rosieleserpa@hotmail.com>
2021-05-19 16:38:36 -0300

    criaçao da paginaçao

--- js-test/src/context/GlobalConsumer.js
+++ js-test/src/context/GlobalConsumer.js
@@ -5,25 +5,30 @@ import { requestRepos } from '../services/api';
 
 function GlobalProvider({ children }) {
   const [repos, setRepos] = useState([]);
+  const [userRepos, setUserRepos] = useState();
   const [searchValue, setSearchValue] = useState('');
   const [searchType, setSearchType] = useState('');
   const [repoDetailsCommits, setRepoDetailsCommits] = useState();
   const [repoDetailsBranches, setRepoDetailsBranches] = useState();
   const [repoDetailsLastUpdate, setRepoDetailsLastUpdate] = useState();
+  const [offset, setOffset] = useState(0);
 
   useEffect(async () => {
     const repositories = await requestRepos();
     setRepos(repositories);
+    setUserRepos(repositories[0].owner.login);
   }, []);
 
   const provide = {
     values: {
       repos,
+      userRepos,
       searchValue,
       searchType,
       repoDetailsCommits,
       repoDetailsBranches,
       repoDetailsLastUpdate,
+      offset,
     },
     functions: {
       setSearchValue,
@@ -31,6 +36,7 @@ function GlobalProvider({ children }) {
       setRepoDetailsCommits,
       setRepoDetailsBranches,
       setRepoDetailsLastUpdate,
+      setOffset,
     },
   };
 
