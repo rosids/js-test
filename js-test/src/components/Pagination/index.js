e1cba0ee14f015513806c383645df2ad6c589dcf
Rosiele David <rosieleserpa@hotmail.com>
2021-05-19 23:39:30 -0300

    adiona botao de proximo e anterior na paginaçao

--- js-test/src/components/Pagination/index.js
+++ js-test/src/components/Pagination/index.js
@@ -1,25 +1,41 @@
 /* eslint-disable react/prop-types */
 import React, { useContext } from 'react';
 import GlobalContext from '../../context/GlobalContext';
+import style from './style.module.css';
 
 export default function Pagination({ array, pageSize }) {
-  const { functions: { setOffset } } = useContext(GlobalContext);
+  const { values: { offset }, functions: { setOffset } } = useContext(GlobalContext);
 
   const getPageNumbers = () => [...new Array(Math.ceil(array.length / pageSize))]
     .map((_, index) => index);
 
+  function handleIncreaseOffsetNextButton() {
+    const newOffset = offset + pageSize > array.length - 1 ? offset : offset + pageSize;
+    setOffset(newOffset);
+  }
+
+  function handleDecreaseOffsetPreviousButton() {
+    const newOffset = Math.max(0, offset - pageSize);
+    setOffset(newOffset);
+  }
+
+  const currentPage = offset === 0 ? 1 : Math.ceil((offset / pageSize) + 1);
+
   return (
-    <ul>
+    <div className={style.pagination}>
+      <button type="button" onClick={handleDecreaseOffsetPreviousButton}>Anterior</button>
       {getPageNumbers()
         .map((page) => (
           <button
             type="button"
             key={page}
+            className={page === currentPage - 1 ? style.currentPage : style.pagination}
             onClick={() => setOffset(page * pageSize)}
           >
             {page + 1 }
           </button>
         ))}
-    </ul>
+      <button type="button" onClick={handleIncreaseOffsetNextButton}>Próxima</button>
+    </div>
   );
 }
