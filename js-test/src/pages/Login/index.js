ad6f1d545d736aa09a60a071be09a1b033c7435b
Rosiele David <rosieleserpa@hotmail.com>
2021-05-20 11:25:16 -0300

    ajusta layout da pagina de login

--- js-test/src/pages/Login/index.js
+++ js-test/src/pages/Login/index.js
@@ -1,35 +1,48 @@
 import React, { useState } from 'react';
-import './index.css';
+import { Link } from 'react-router-dom';
+import style from './style.module.css';
 
 export default function Login() {
-  const [email, setEmail] = useState();
-  const [password, setPassword] = useState();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
   const handleEmail = ({ target }) => setEmail(target.value);
   const handlePassword = ({ target }) => setPassword(target.value);
 
+  const validateLogin = () => {
+    const isEmail = /\S+@\S+\.\S+/;
+    const MINIMUM_PASSWORD_SIZE = 6;
+    return !isEmail.test(email) || password.length < MINIMUM_PASSWORD_SIZE;
+  };
+
   return (
-    <form className="form">
-      <label htmlFor="email">
-        Email:
+    <div className={style.loginContainer}>
+      <form className={style.form}>
         <input
           type="email"
           name="email"
           value={email}
           id="email"
           onChange={handleEmail}
+          placeholder="nome@email.com"
         />
-      </label>
-      <label htmlFor="password">
-        Senha:
         <input
           type="password"
           name="password"
           value={password}
           id="password"
           onChange={handlePassword}
+          placeholder="senha"
         />
-      </label>
-    </form>
+        <Link to="/projects">
+          <button
+            type="submit"
+            disabled={validateLogin()}
+          >
+            Entrar
+          </button>
+        </Link>
+      </form>
+    </div>
   );
 }
